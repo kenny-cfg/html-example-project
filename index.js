705eb900a3848559270b5b89eb442b306e50552c
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-08 20:08:52 +0100

    Back to a basic example

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-document.getElementById('top-heading').innerText = 'This is coming from JavaScript'
-const paragraphElements = document.getElementsByClassName('poo')
-for (const paragraphElement of paragraphElements) {
-  paragraphElement.style.backgroundColor = 'blue'
-}
-
-const buttonElement = document.getElementById('button')
-buttonElement.onclick = function() {
-  console.log('HELP! I need somebody')
-}
+console.log('HELLO')
