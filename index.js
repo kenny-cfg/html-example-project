ddcb8255fe828a69759f73cdb6e6d5151a7e5f4b
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-08 20:11:36 +0100

    Increment counter

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let counter = 0
 displayCounter()
 
+document.getElementById('increase').onclick = incrementCounter
+
 function displayCounter() {
   document.getElementById('counter').innerText = counter
+}
+
+function incrementCounter() {
+  counter = counter + 1
+  displayCounter()
 }
