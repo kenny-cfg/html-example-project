4a64b4f4180c150f9ecca2837f2ed3be30c528f0
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-08 20:13:33 +0100

    Decrease counter

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let counter = 0
 displayCounter()
 
 document.getElementById('increase').onclick = incrementCounter
+document.getElementById('decrease').onclick = decreaseCounter
 
 function displayCounter() {
   document.getElementById('counter').innerText = counter
@@ -9,5 +10,10 @@ function displayCounter() {
 
 function incrementCounter() {
   counter = counter + 1
+  displayCounter()
+}
+
+function decreaseCounter() {
+  counter = counter - 1
   displayCounter()
 }
