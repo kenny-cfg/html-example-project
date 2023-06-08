42a109ea9bd5fd47c3238d90006de37e18fe3aaf
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-08 20:01:36 +0100

    Some DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('RUNNING')
+document.getElementById('top-heading').innerText = 'This is coming from JavaScript'
+const paragraphElements = document.getElementsByClassName('poo')
+for (const paragraphElement of paragraphElements) {
+  paragraphElement.style.backgroundColor = 'blue'
+}
+
+
+/*
+DOM - Document Object Model
+*/
